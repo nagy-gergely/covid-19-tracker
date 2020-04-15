d42ebfba3bbd5633a45231e87ccb4c20d45dc18d
Nagy Gergely <nagy.gergely.9211@gmail.com>
2020-04-15 17:09:04 +0200

    Updates

---
+++ precache-manifest.f044ec0a8e05715eba3a745635907037.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5ba4a03f6a41b8ea895d8c16a3fc730",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "ed4b02affd1cf6940af4",
+    "url": "/covid-19-tracker/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "fecddf9c450df2d0558f",
+    "url": "/covid-19-tracker/static/css/main.6b0f65af.chunk.css"
+  },
+  {
+    "revision": "ed4b02affd1cf6940af4",
+    "url": "/covid-19-tracker/static/js/2.3d2b01e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-19-tracker/static/js/2.3d2b01e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fecddf9c450df2d0558f",
+    "url": "/covid-19-tracker/static/js/main.ba6fe1f5.chunk.js"
+  },
+  {
+    "revision": "f058a53e7701713c744a",
+    "url": "/covid-19-tracker/static/js/runtime-main.64c115b7.js"
+  }
+]);
