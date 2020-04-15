899f740f9b6c58a098058b7113dac620ae9eb445
Nagy Gergely <nagy.gergely.9211@gmail.com>
2020-04-15 14:32:16 +0200

    add total chart

---
+++ src/TotalChart.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import Chart from "react-google-charts";
+
+function TotalChart() {
+    const [data, setData] = useState([['Date', 'Confirmed', 'Deaths']]);
+
+    const options = {
+        title: "Total Confirmed Cases & Total Deaths",
+        curveType: "function",
+        legend: { position: "top" }
+      };
+
+    useEffect(() => {
+        axios.get('https://covid19.mathdro.id/api/daily')
+            .then((response) => {
+                console.log(response);
+                let newData = [...data];
+                if(response.data && Array.isArray(response.data)) {
+                    response.data.forEach(({ reportDate, confirmed, deaths }) => {
+                        newData.push([reportDate, confirmed.total, deaths.total])
+                    });
+                    setData(newData);
+                } 
+            })
+            .catch((err) => {
+                console.error(err.message)
+            });
+    }, []);
+
+    return (
+        <div className="row">
+            <Chart
+                chartType="LineChart"
+                width="100%"
+                height="400px"
+                data={data}
+                options={options}
+            />
+        </div>
+    );
+}
+
+export default TotalChart;
